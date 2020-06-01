ad75a7c64bd37954b9d3c1b3f689cd3093ba65ed
therajeshkumar <web.rajeshkumar18@gmail.com>
2020-06-01 12:05:39 +0530

    first

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 app.get('/', (req, res) => { res.sendFile(__dirname + '/index.html') });
 
-app.get('/about', (req, res) => { res.sendFile(__dirname + '/about.html') });
 
 
 
